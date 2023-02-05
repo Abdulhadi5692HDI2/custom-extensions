9d6799a06ad388e448055bf61c4ab0ea3e067c24
Abdulhadi5692HDI2 <110974220+Abdulhadi5692HDI2@users.noreply.github.com>
2023-02-05 19:49:44 +0000

    Update main.js

--- RandomNumber/main.js
+++ RandomNumber/main.js
@@ -3,6 +3,9 @@ class RNumber {
     return {
       id: 'otherstuff',
       name: 'Random Number',
+      
+      menuIconURI: 'https://abdulhadi5692hdi2.github.io/custom-extensions/RandomNumber/icon.png',
+      docsUri: 'https://abdulhadi5692hdi2.github.io',
 
       blocks: [
         {
